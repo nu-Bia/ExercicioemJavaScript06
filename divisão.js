110366c9c37cdc269945315980e8b00b6f996b6d
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-10-05 15:14:17 -0300

    Add files via upload
    6) Escreva um algoritmo para ler dois valores do teclado e exibir como resposta a divisão dos valores.

---
+++ divisão.js
@@ -0,0 +1,6 @@
+function divida(){
+    var numero1=prompt(("digite um numero:"));
+    var numero2=(prompt("digite um numero:"));
+    var divida=numero1/numero2;
+    alert(" O resultado da divisão é :"+divida);
+    }
